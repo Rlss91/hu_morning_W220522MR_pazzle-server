4af8686e89d607cbf207977dfcab19c7bb3682fb
Rlss91 <rlss91@gmail.com>
2022-12-13 15:31:33 +0200

    add react build

--- app.js
+++ app.js
@@ -13,5 +13,10 @@ app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/api", apiRouter);
+/* React */
+app.use(express.static(path.join(__dirname, "build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 module.exports = app;
